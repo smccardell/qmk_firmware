feef6d2a231938cfd3c2f107afa111d632ea8e2f
shawn <smccardell@gmail.com>
2025-10-02 22:05:36 -0500

    initializimg totem

---
+++ keyboards/totem/keymaps/tech5un/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MASTER_LEFT
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// #define TAPPING_TERM 170
+#define TAPPING_TERM 300
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 120
+
+// all keys pressed within 150ms are considered a tap
+#define FLOW_TAP_TERM 150
